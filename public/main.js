2f8088940c0ec0ecbf5f2c77ba541a4947e4980f
Tehya Hubbert <taye@Tehyas-MacBook-Pro.local>
2019-04-11 00:47:52 -0400

    styled index.ejs

--- public/main.js
+++ public/main.js
@@ -1,4 +1,3 @@
-// do: html and little(?) css first
 // user must log in/create an account. allow: google log in, local log in, pintrest(?)
 
 // vvv outside api vvv
@@ -8,9 +7,30 @@
 // three collections: user (dont touch), colors (based on user session), palettes (based on user session)
 
 // vvv own api vvv
-// save button function fetches to own api (post) and saves the color in specific user's post (ask if you can have the property be the color name in the db? or how one collection will contain all one user's color?)
-// color log can be shown within the popup on another tab || on website on its own tab. displays all the color documents in the color db
-// on color log page: render small img, hex code, rgb code and above all that is a "make palette" button.
-// "make palette" button function redirects to palette creation page which is a form that makes another post req to own api of all the colors in the palette (name, rgb, hex), name of palette, optional description and the src of the palette img.
+// save button function fetches to own api (post) and saves the color in specific user's post (do i have a seperate collection for each user's color or do i utilize the user IDs?)
+
+// ========= after user login page ========== //
+//as chrome extension pop up
+//button to activate the color picker //click again gives color info aka preview //second click saves color into color log
+//display color log in a new tab in the pop up?? so user doesnt save the same color
+
+// ============ color log page ============= //
+//make color palette and palette gallery buttons at the top. redirect to respected pages
+//display colors from specific user in colors collection on the page
+//color collection must contain: color visual (img), color name(?), rgb and hex codes. (retrieved from color api then sent to own api/color collection db, then displayed into color log)
+//delete req: delete saved colors
+
+// ============ make palette page ============= //
+
+// "make palette" button function redirects to palette creation page which is a form that makes another post req to own api
+
+// render color documents in color collection for specific user for the user to PICK colors from
+// user can pick colors, create palette name, optional palette description
+//on the click of create button: take the selected colors from color collection - take color code (rgb or hex) combine and display into one image, send to routes to create a new doc in palettes collection
+//create a new document in palettes collection containing the newly made palette image, palette name and optional description.
+
+
+// ============ palettes gallery page ========== //
+//display all palettes from user in palettes collection on the page
 // update req: change palette name && || description
-// delete: delete palettes or saved colors
+// delete: delete palettes
